f1b1261af6cef712441a647e9f1f46191ab04841
Ben <benjamin.main88@gmail.com>
2020-12-07 13:53:30 +0000

    App displays current scores for each player

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// initial state
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const initial = {
+    player1: 0,
+    player2: 0,
+};
+
+// reducer
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// store
+
+const store = createStore(reducer, initial);
+
+const render = () => {
+    let state = store.getState();
+
+    ReactDOM.render(
+        <React.StrictMode>
+            <App player1={state.player1} player2={state.player2} />
+        </React.StrictMode>,
+        document.getElementById("root")
+    );
+};
+
+store.subscribe(render);
+render();
+
+// render
